735d7b1584cf41d762e70e6d8d540b56ff38556e
blind-quist <120838665+blind-quist@users.noreply.github.com>
2024-08-27 12:36:15 -0700

    Add files via upload

--- main.m
+++ main.m
@@ -11,13 +11,13 @@ T_inlet = 300; % K
 k_w = 20; %W/m K
 res = 0; % thermal resistance coating
 Thrust = 1600; % lbf
-Pe = 13; % psi
+%Pe = 13; % psi 
 C_star_eff = 0.94;
 C_F_eff = 0.9;
 L_star = 35; % in
 h_ch = 0.001/0.0254; % in
 w_ch_min = 0.001/0.0254; % in
-% num_ch = 50;
+%num_ch = 50;
 w_rib = 0.001/0.0254; % in
 D_c = 3.875; % in
 t_ins = 0.001/0.0254; % in
@@ -48,7 +48,44 @@ ratio = 0.75;
 
 [T_c,T_sat_c,P_c,q,T_chg,T_rhg,T_ci,T_ri,T_cb,T_rb,T_rt,T_ro,T_co,T_ct] = heatTransfer2D(Pc,M,P,A,D,D_h,w_ch,h_ch,w_rib,num_ch,t_ins,t_out,step,pos,gamma,mu_g,Cp_g,k_g,Pr_g,C_star,T,k_w,D_t,A_t,T_inlet,ratio,mdot_f,C_star_eff,res);
 
-[stress_total,stress_T_ow, stress_T_iw, stress_T_s, stress_T_c,stress_P_b, stress_P_c, stress_P_s, stress_P_t, stress_P_hoop, stress_P_a,yield] = stress(T_ci,T_co,P,P_c,A,D,w_ch,w_rib,t_ins,h_ch,t_out,num_ch);
+
+% ===STRESS===
+% Temperature Dependent Material properties
+% Is averaging these temps the right approach???
+% Inner Wall
+T_iw = (T_rhg+T_chg)/2;
+T_iw = double(T_iw);
+[row_iw,E_iw,nu_iw,alpha_iw,k_iw,Cp_iw,Yield_iw] = MatProperties_Steel174ph(T_iw);
+% Outer Wall
+T_ow = (T_ro+T_co)/2;
+T_ow = double(T_ow);
+[row_ow,E_ow,nu_ow,alpha_ow,k_ow,Cp_ow,Yield_ow] = MatProperties_Steel174ph(T_ow);
+
+% TEMPORARY VARIABLE NAME CONVERSION (Will update code)
+t_iw = t_ins;
+t_ow = t_out;
+N = num_ch;
+h = h_ch;
+w = w_rib;
+d = w_ch_min;
+r_iw = D_c/2;
+r_ow = r_iw+t_iw+d+t_ow;
+deltaP = 206843;  % Pressure difference [Pa] channel(higher) to chamber (30  psi)
+P_chamber = Pc*6895; % [psi to Pa]
+A_channels = N*h*d;
+A_wall = pi*(r_ow.^2 - r_iw.^2) - A_channels;
+A_chamber = pi*r_iw.^2;
+A_throat = pi*(D_t/2).^2;
+A_cc = A_chamber-A_throat;
+
+% Conley's function call
+%[stress_total,stress_T_ow, stress_T_iw, stress_T_s, stress_T_c,stress_P_b, stress_P_c, stress_P_s, stress_P_t, stress_P_hoop, stress_P_a,yield] = stress(T_ow,T_iw,E_iw,E_ow,alpha_iw,alpha_ow,nu_iw,nu_ow,P,P_c,A,D,w_ch,w_rib,t_ins,h_ch,t_out,num_ch);
+% Brandon's function call
+[stressTotal,stressTow, stressTiw, stressTs, stressTc, stressPb, stressPs, stressPt, stressP_hoopow, stressP_hoopiw, stressPa,vonMises] = stress(T_iw,T_ow,E_iw,E_ow,alpha_iw,alpha_ow,nu_iw,nu_ow,t_iw,t_ow,h,w,d,r_ow,r_iw,deltaP,P_chamber,A_wall,A_cc)
+
+% Total Inner wall stress only
+stressTotaliw = stressTiw + stressP_hoopiw;
+% ===========
 
 
 P_c = P_c/6894.76; % psi
@@ -123,12 +160,12 @@ clf
 hold on
 xline(pos(id_th), '--', 'Throat','HandleVisibility','off')
 xline(pos(id_c), '--', 'Chamber','HandleVisibility','off')
-plot(pos,stress_total/6895000,'LineWidth',1)
-plot(pos,yield/6895000,'LineWidth',1)
+plot(pos,stressTotaliw/6895000,'LineWidth',1)
+plot(pos,Yield_iw/6895000,'LineWidth',1)
 xline(0, '--', 'Exit','HandleVisibility','off')
 hold off
 title('Stress')
 xlabel('Axial Distance (m)')
 ylabel('Stress (ksi)')
-legend('Stress','Yield')
+legend('Inner Wall Stress','Inner Wall Yield')
 grid on
--- stress.m
+++ stress.m
@@ -1,60 +1,64 @@
-function [stress_total,stress_T_ow, stress_T_iw, stress_T_s, stress_T_c,stress_P_b, stress_P_c, stress_P_s, stress_P_t, stress_P_hoop, stress_P_a,yield_out] = stress(T_ci,T_co,P,P_c,A,D,b,w,t,h,d,num_ch)
+function [stressTotal,stressTow, stressTiw, stressTs, stressTc,stressPb, stressPs, stressPt, stressP_hoopow,stressP_hoopiw, stressPa,vonMises] = stress(T_iw,T_ow,E_iw,E_ow,alpha_iw,alpha_ow,nu_iw,nu_ow,t_iw,t_ow,h,w,d,r_ow,r_iw,deltaP,P_chamber,A_wall,A_cc)
+%{ =====STRESS===== 
+% Total Stress calculation at each cross section
+
+% Sources: 
+% (1) DEVELOPMENT OF ADVANCED FABRICATION TECHNIQUES FOR REGENERATIVELY COOLED THRUST CHAMBERS
+%}
+
+% Define arrays
+% [stress_T_ow, stress_T_iw, stress_T_s, stress_T_c] = deal(zeros(1,numel));
+% [stress_P_b, stress_P_c, stress_P_s, stress_P_t, stress_P_hoop, stress_P_a] = deal(zeros(1,numel));
+
+% Average inner and outer wall
+E_avg = (E_iw+E_ow)/2;
+alpha_avg = (alpha_iw+alpha_ow)/2;
+nu_avg = (nu_iw+nu_ow)/2;
 
 
-    t = t*ones(1,length(A));
-    d = d*ones(1,length(A));
-    w = w*ones(1,length(A));
-    h = h*ones(1,length(A));
-    for i=1:length(A)
-        % MATREIAL PROPERTIES
-        [E_iw,nu_iw,alpha_iw,k_iw,yield_iw] = materialPropertiesSteel174PH(T_ci(i));
-        [E_ow,nu_ow,alpha_ow,k_ow,yield_ow] = materialPropertiesSteel174PH(T_co(i));
-        E = 0.5*(E_iw+E_ow);
-        nu = 0.5*(nu_iw+nu_ow);
-        alpha = 0.5*(alpha_iw+alpha_ow);
-        k = 0.5*(k_iw+k_ow);
-        yield = 0.5*(yield_iw+yield_ow);
-        
-        % THERMAL STRESS [Pa]
-        % Circumferential stress outer wall  [(1)B3,p120] 
-        stress_T_ow(i) = ((alpha_ow*E_ow*(T_ci(i)-T_co(i)))/(1-nu_ow))*(t(i)/(t(i)+d(i)));
-        % Circumferential stress inner wall [(1)B4,p121] 
-        stress_T_iw(i) = ((alpha_iw*E_iw*(T_ci(i)-T_co(i)))/(1-nu_iw))*(d(i)/(t(i)+d(i)));
-        % Shear Stress max [(1)B4,p121]
-        stress_T_s(i) = ( (alpha*E*(T_ci(i)-T_co(i)))/(5*w(i)*(1-nu)) ) * ((t(i)*d(i))/(t(i)+d(i))^2) * (b(i)+w(i));
-        % Compression Stress max [(1)B4,p121]
-        stress_T_c(i) = ( (alpha*E*(T_ci(i)-T_co(i))) / ((w(i)*(D(i)/2+t(i)+h(i)+d(i))/(b(i)+w(i)))*(1-nu))) * ((d(i)*t(i))/(t(i)+d(i)));
-        
-        % PRESSURE STRESS: INNER WALL [Pa]
-        % Maximum bending stress  [(1)A3,p111]
-        stress_P_b(i) = ((P_c(i)-P(i))/2)*(b(i)/t(i))^2;
-        % Compressive loading, ribs
-        stress_P_c(i) = ((P_c(i)-P(i))/w(i))*(b(i)+w(i));
-        % Tensile stress, ribs
-        stress_P_t(i) = ((P_c(i)-P(i))/w(i))*b(i);
-        % Shear stress, ribs
-        stress_P_s(i) = ((P_c(i)-P(i))/2)*(b(i)/t(i));
-        % circumferential stress in the inner and outer walls
-        stress_P_hoop(i) = (P_c(i)-P(i))*(D(i)/2+t(i)+h(i)+d(i))/(t(i)+d(i));
-        % Hoop 
-        stress_P_hoop(i) = (P_c(i)+P(i))*(D(i)/2)/(t(i)+d(i));
-        % Axial 
-        stress_P_a(i) = (P_c(i)-P(i))*A(i)/(pi*(D(i)/2+t(i)+h(i)+d(i))^2-A(i)-(num_ch*b(i)*h(i)));
-        % if stress_P_a(i) < 0
-        %     stress_P_a(i) = 0;
-        % end
-        yield_out(i) = yield;
-    
-    end
-    
-    
-    % TOTAL STRESS
-    % these stresses can be simply added together to determine the total stress on an object because of the superposition principle.
-    stress_total = stress_T_ow + stress_T_iw + stress_T_s + stress_T_c + stress_P_b + stress_P_c + stress_P_s + stress_P_t + stress_P_hoop + stress_P_a;
-    
-    % Von Mises maximum distortion energy theory
-    % failure by yielding occurs when, at any point in the body, the distortion energy per unit volume 
-    % in a state of combined stress becomes equal to that associated with yielding
+% THERMAL STRESS [Pa]
+% Circumferential stress outer wall  [(1)B3,p120] ()
+stressTow = ((alpha_ow.*E_ow.*(T_iw-T_ow))./(1-nu_ow)).*(t_iw./(t_iw+t_ow));
+% Circumferential stress inner wall [(1)B4,p121]  ()
+stressTiw = ((alpha_iw.*E_iw.*(T_iw-T_ow))./(1-nu_iw)).*(t_ow/(t_iw+t_ow));
+% Shear Stress max [(1)B4,p121] ()
+stressTs = ( (alpha_avg.*E_avg.*(T_iw-T_ow))./(5.*w.*(1-nu_avg)) ) .* ((t_ow.*t_iw)/(t_iw+t_ow).^2) .* (h+w);
+% Compression Stress max [(1)B4,p121]  ()
+stressTc = ( (alpha_avg.*E_avg.*(T_iw-T_ow)) ./ ((w.*r_ow/(h+w)).*(1-nu_avg)) ) .* ((t_ow.*t_iw)/(t_iw+t_ow));
+
+% PRESSURE STRESS: INNER WALL [Pa]
+% Maximum bending stress  [(1)A3,p111] ()
+stressPb = (deltaP/2).*(h/t_iw)^2;
+
+    % If Chamber pressure greater than coolant preasure: Compressive loading, ribs  (ignore for von mises)
+    % stressPc = (deltaP/w).*(h+w);
+    % (Expecting this) If Chamber pressure less than coolant preasure: Tensile stress, ribs (ignore for von mises)
+    stressPt = (deltaP/w).*h;
+
+% Shear stress, ribs (ignore for von mises)
+stressPs = (deltaP/2).*(h/t_iw);
+% circumferential stress in the inner and outer walls ()
+stressP_hoopow = deltaP.*r_ow./(t_iw+t_ow);
+stressP_hoopiw = deltaP.*r_iw./(t_iw+t_ow);
+% Axial 
+stressPa = P_chamber.*A_cc./A_wall;
+
+
+% TOTAL STRESS
+% these stresses can be simply added together to determine the total stress on an object because of the superposition principle.
+stressTotal = stressTow + stressTiw + stressTs + stressTc + stressPb + stressPs + stressP_hoopow + stressP_hoopiw + stressPa;
+
+% Von Mises maximum distortion energy theory
+% failure by yielding occurs when, at any point in the body, the distortion energy per unit volume 
+% in a state of combined stress becomes equal to that associated with yielding
+% Failure occurs when the von Mises stress exceeds the material's 0.2% yield strength.
+% https://engrapps.com/mechanical-systems-and-materials/mechanical-components/pressure-vessels/burst-collapse-analysis.php
+stressTotalAxial = stressPa;
+stressTotalRadial = stressTc + stressPb;
+stressTotalHoop = stressTow + stressTiw + stressP_hoopow + stressP_hoopiw;
+stressTotalTow = stressTs;
+
+vonMises = sqrt( (1/2).*((stressTotalAxial-stressTotalRadial).^2 + (stressTotalRadial-stressTotalHoop).^2 + (stressTotalHoop-stressTotalAxial).^2 )+3.*stressTotalTow.^2);
 
 end
 
